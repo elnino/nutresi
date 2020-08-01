1a7fc5e7a44ea92c58762645f7f95fe6eb58b1ec
Wido Raharjo <hielnino@gmail.com>
2020-08-01 15:16:35 +0700

    Fix: Full height.

--- themes/nutresi/static/js/full.js
+++
@@ -1,8 +0,0 @@
-// Fix Full Height on Mobile
-
-function full() {
-  document.documentElement.style.setProperty('--vh', `${document.documentElement.clientHeight * 0.01}px`);
-}
-
-window.addEventListener('resize', full);
-window.addEventListener('orientationchange', full);
